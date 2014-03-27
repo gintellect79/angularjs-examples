68fbbd7987adb0ddba40a5857e4308edae5e2002
gintellect@gmail.com <gintellect@gmail.com>
2014-03-27 11:12:30 +0530

    Comment out first function.

--- ex0.js
+++ ex0.js
@@ -1,6 +1,6 @@
 (function(angular) {
-	var first = function() {
-		// This is a function from branch 1.
+	// var first = function() {
+	// This is a function from branch 1.
 	var second = function() {
 		// This is a function from branch 2.
 	};

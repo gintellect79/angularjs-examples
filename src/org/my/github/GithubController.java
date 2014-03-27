2f5a5a6994f6f5fd55903ce7fe6b5e3d8aa7045a
gintellect@gmail.com <gintellect@gmail.com>
2014-03-27 10:42:18 +0530

    Removed getFirstBranch method.

--- src/org/my/github/GithubController.java
+++ src/org/my/github/GithubController.java
@@ -1,9 +1,9 @@
 package org.my.github;
 
 public class GithubController {
-	public int getFirstBranch() {
-		return 1;
-	}
+//	public int getFirstBranch() {
+//		return 1;
+//	}
 
 	public int getSecondBranch() {
 		return 2;
